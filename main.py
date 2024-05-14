9c00844988ce206c8f35281b767831cdc838d40f
Chenan <572453111@qq.com>
2024-05-14 21:52:30 +0800

    514 save_loss
    to save loss as png
    and change batch_size into 8

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import time
-from cat_model import cat_model
 from ae_model import ae_model
 import numpy as np
 import pandas as pd
@@ -16,6 +15,7 @@ from urllib.request import urlopen
 from train import train
 from datetime import timedelta
 from collections import defaultdict
+import matplotlib.pyplot as plt
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 from image_dataset import image_dataset
 
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     trainDatasets = image_dataset("./dataset/train.csv", "./dataset/train", dataTransformsTrain)
     validDatasets = image_dataset("./dataset/valid.csv", "./dataset/valid", dataTransformsValid)
     dataloadersTrain = torch.utils.data.DataLoader(trainDatasets,
-                                                   batch_size=4,
+                                                   batch_size=8,
                                                    shuffle=True,
                                                    num_workers=8,
                                                    pin_memory=True,
@@ -57,17 +57,10 @@ if __name__ == '__main__':
 
     # set optimization function
     #optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    initial_learning_rate = 0.001  # **************学习率比例可以调，一般调高收敛慢一点****************
-    lr_schedule = ExponentialDecay(
-        initial_learning_rate=initial_learning_rate,
-        decay_steps=300,
-        decay_rate=0.9,
-        staircase=False
-    )
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-5 )
-    ExpLR = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01 )
+    ExpLR = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
     criterion = nn.MSELoss(reduction='mean')
 
     # training
-    model_ft = train(model, dataloadersTrain, dataloadersValid, optimizer, criterion, 10)
+    model_ft = train(model, dataloadersTrain, dataloadersValid, optimizer, criterion, 50)
 
